94024ebb4266e15eb78224530be715b3b155c8ca
Kongesque <Kongesque@gmail.com>
2024-12-18 22:29:26 +0800

    Update code with new changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 
-// import SnakeGame from '../components/snake';
-const SnakeGame = dynamic(() => import('../components/snake'), { ssr: false });
+import SnakeGame from '../components/snake';
+// const SnakeGame = dynamic(() => import('../components/snake'), { ssr: false });
 
 const Home: NextPage = () => {
   return (
     <main>
       <div className="flex flex-col items-center justify-center h-screen mx-auto max-w-2xl px-6">
-        <h1 className="text-3xl sm:text-4xl font-bold xanh-mono-font mb-8">A Perfect Snake AI</h1>
+      <h1 className="text-3xl sm:text-4xl libre-caslon-font mb-8 transform scale-y-[1.25]">A Perfect Snake AI</h1>
         <SnakeGame />
         <p className="geist-mono-font text-center text-sm sm:text-base mt-8 mb-4">
           Uses A* pathfinding and a Hamiltonian cycle for flawless play.
