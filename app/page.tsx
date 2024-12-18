f357bb0bd0db3b54fc1bdd6493d34af6e4a8e5ee
Kongesque <Kongesque@gmail.com>
2024-12-18 13:45:33 +0800

    Update code with new changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,26 @@
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 
-// import SnakeGame from './snake'; 
-const SnakeGame = dynamic(() => import('../components/snake'), { ssr: false }); 
+// import SnakeGame from '../components/snake';
+const SnakeGame = dynamic(() => import('../components/snake'), { ssr: false });
 
 const Home: NextPage = () => {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center h-screen mx-auto max-w-xl px-6">
-        <h1 className="text-3xl sm:text-4xl font-bold mb-8 xanh-mono-font">A Perfect Snake AI</h1>
+      <div className="flex flex-col items-center justify-center h-screen mx-auto max-w-2xl px-6">
+        <h1 className="text-3xl sm:text-4xl font-bold xanh-mono-font mb-6">A Perfect Snake AI</h1>
         <SnakeGame />
+        <p className="geist-mono-font text-center text-sm sm:text-base mt-6 mb-2">
+          Uses A* pathfinding and a Hamiltonian cycle for flawless play.
+        </p>
+        <a
+          href="https://github.com/Kongesque/perfect-snakeAI"
+          className="inline-block hover:text-accent transition-colors geist-mono-font underline text-sm sm:text-base"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Source
+        </a>
       </div>
     </main>
   );
