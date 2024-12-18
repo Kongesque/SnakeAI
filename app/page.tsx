b98f5007e82b5cfe1c2c14ba9bd91d0da90fbf28
Kongesque <Kongesque@gmail.com>
2024-12-18 13:56:57 +0800

    Adjust Spacing Ratios

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ const Home: NextPage = () => {
   return (
     <main>
       <div className="flex flex-col items-center justify-center h-screen mx-auto max-w-2xl px-6">
-        <h1 className="text-3xl sm:text-4xl font-bold xanh-mono-font mb-6">A Perfect Snake AI</h1>
+        <h1 className="text-3xl sm:text-4xl font-bold xanh-mono-font mb-8">A Perfect Snake AI</h1>
         <SnakeGame />
-        <p className="geist-mono-font text-center text-sm sm:text-base mt-6 mb-2">
+        <p className="geist-mono-font text-center text-sm sm:text-base mt-8 mb-4">
           Uses A* pathfinding and a Hamiltonian cycle for flawless play.
         </p>
         <a
